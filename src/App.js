725080ec94482a9bba8f90a4dfb1c8611d9a872a
Lachee <lachlan.h@hotmail.com>
2022-04-28 12:10:35 +1000

    Removed big spinny logo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Search engine="ddg"></Search>
         <ShortcutList links={links}></ShortcutList>
         <a
