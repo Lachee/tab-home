644c7e6b020c28941824c4a323633c65c3a584b6
Lachee <lachlan.h@hotmail.com>
2022-05-12 15:17:33 +1000

    WIP: Basic TimePiece structure

--- src/pages/clock/Clock.js
+++ src/pages/clock/Clock.js
@@ -5,6 +5,8 @@ import React, { useRef } from "react";
 
 export class Clock extends React.Component {
 
+    /** @type {TimePiece} the current timepiece */
+    timepiece;
     canvas;
 
     constructor(props) {
@@ -12,120 +14,69 @@ export class Clock extends React.Component {
         this.canvas = React.createRef();
     }
 
-    draw() {
-        console.log('draww');
-
-        let canvas = this.canvas.current;
-        if (!canvas) canvas = document.getElementById('fishsticks');
-
-        /** @type {CanvasRenderingContext2D} */
-        const ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        
-        const radius = (canvas.height / 2);
-        ctx.translate(radius, radius);
-        this.drawFace(ctx, radius);
-        this.drawNumbers(ctx, radius);
-        this.drawTime(ctx, radius);
-    }
-
-    /**
-     * Renders the face of the clock
-     * @param {CanvasRenderingContext2D} ctx 
-     * @param {number} radius 
-     */
-    drawFace(ctx, radius) {
-        var grad;
-        ctx.beginPath();
-        ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-        grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
-        grad.addColorStop(0, '#333');
-        grad.addColorStop(0.5, 'white');
-        grad.addColorStop(1, '#333');
-        ctx.strokeStyle = grad;
-        ctx.lineWidth = radius * 0.1;
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
-        ctx.fillStyle = '#333';
-        ctx.fill();
-    }
-
-    /**
-     * Renders the numbers of a clock to the context
-     * @param {CanvasRenderingContext2D} ctx 
-     * @param {number} radius 
-     */
-    drawNumbers(ctx, radius) {
-        var ang;
-        var num;
-        ctx.font = radius * 0.15 + "px arial";
-        ctx.textBaseline = "middle";
-        ctx.textAlign = "center";
-        for (num = 1; num < 13; num++) {
-            ang = num * Math.PI / 6;
-            ctx.rotate(ang);
-            ctx.translate(0, -radius * 0.85);
-            ctx.rotate(-ang);
-            ctx.fillText(num.toString(), 0, 0);
-            ctx.rotate(ang);
-            ctx.translate(0, radius * 0.85);
-            ctx.rotate(-ang);
-        }
-    }
-
-    /**
-     * Renders the current time on the clock
-     * @param {CanvasRenderingContext2D} ctx 
-     * @param {number} radius 
-     */
-    drawTime(ctx, radius) {
-        var now = new Date();
-        var hour = now.getHours();
-        var minute = now.getMinutes();
-        var second = now.getSeconds();
-        //hour
-        hour = hour % 12;
-        hour = (hour * Math.PI / 6) +
-            (minute * Math.PI / (6 * 60)) +
-            (second * Math.PI / (360 * 60));
-        this.drawHand(ctx, hour, radius * 0.5, radius * 0.07);
-
-        //minute
-        minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
-        this.drawHand(ctx, minute, radius * 0.8, radius * 0.07);
-
-        // second
-        second = (second * Math.PI / 30);
-        this.drawHand(ctx, second, radius * 0.9, radius * 0.02);
-    }
-
-    /**
-     * Renders a non-interactable hand to the clock
-     * @param {CanvasRenderingContext2D} ctx 
-     * @param {number} pos rotation of the hand
-     * @param {number} length length of the hand
-     * @param {number} width thickness of the hand
-     */
-    drawHand(ctx, pos, length, width) {
-        ctx.beginPath();
-        ctx.lineWidth = width;
-        ctx.lineCap = "round";
-        ctx.moveTo(0, 0);
-        ctx.rotate(pos);
-        ctx.lineTo(0, -length);
-        ctx.stroke();
-        ctx.rotate(-pos);
-    }
-
-
     componentDidMount() {
-        this.draw();
+        if (this.timepiece != null) {
+            this.timepiece.detatch();
+        }
+
+        this.timepiece = new TimePiece(this.canvas.current);
     }
 
     render() {
-        return <canvas id='fishsticks' ref={this.canvas} {...this.props} style={{ background: 'white' }}/>
+        return <canvas  ref={this.canvas} {...this.props} style={{ background: 'white' }}/>
     }
+}
+
+class TimePiece {
+
+    /** @type {HTMLCanvasElement} the current canvas */
+    canvas;
+
+    /** @type {CanvasRenderingContext2D} current context */
+    ctx;
+
+    /** current animation frame request id */
+    _request = false;
+
+    constructor(canvas = null) {
+        if (cavnas)
+            this.attach(canvas);
+    }
+
+    /** Renders a frame */
+    renderFrame() {
+        if (this.canvas == null)
+            return; 
+
+        // Queue the next frame
+        this.queueFrame();
+    }
+
+    /** Queues the frame for rendering  */
+    queueFrame() {
+        if (this.canvas == null) 
+            return false;
+        return this._request = window.requestAnimationFrame(() => {
+            this.renderFrame();
+        });
+    }
+
+    attach(canvas) {
+        if (canvas === null) 
+            throw Error('Canvas is null. Did you mean detatch()?');
+        
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+
+        // Kick off the rendering
+        this.queueFrame();
+    }
+
+    detatch() {
+        this.canvas = null;
+        this.ctx = null;
+        if (this._request !== false)
+            window.cancelAnimationFrame(this._request);
+    }
+
 }
