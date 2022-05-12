7eff3c21f9301b2078c958a11b857beba69c657b
Lachee <lachlan.h@hotmail.com>
2022-05-12 16:30:40 +1000

    WIP: Added clock hands

--- src/pages/clock/Clock.js
+++ src/pages/clock/Clock.js
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import { easeInBack, easeOutElastic } from "../../utils/Smoothing";
 
 // tutorial: https://blog.cloudboost.io/using-html5-canvas-with-react-ff7d93f5dc76
 // tutorial: https://reactjs.org/docs/refs-and-the-dom.html#legacy-api-string-refs
@@ -27,8 +28,14 @@ export class Clock extends React.Component {
     }
 }
 
-class TimePiece {
+export const TimePieceSecondMode = {
+    Instant: 'instant',
+    Jittery: 'jitter',
+    Smooth: 'smooth',
+}
+export class TimePiece {
 
+    
     /** @type {HTMLCanvasElement} the current canvas */
     canvas;
 
@@ -38,8 +45,15 @@ class TimePiece {
     /** current animation frame request id */
     _request = false;
 
+    frame = 0;
+
+    secondMode = TimePieceSecondMode.Jittery;
+
+    faceColor = '#AEAEAE';
+    numeralColor = '#000000';
+
     constructor(canvas = null) {
-        if (cavnas)
+        if (canvas)
             this.attach(canvas);
     }
 
@@ -49,7 +63,80 @@ class TimePiece {
             return; 
 
         // Queue the next frame
+        this.frame++;
         this.queueFrame();
+
+        // Start rendering
+        const { ctx, canvas } = this;
+        const { width, height } = canvas;
+        ctx.clearRect(0, 0, width, height);
+        
+        const radius = Math.max(width, height) / 2.0 * 0.8;
+        const x = width / 2.0;
+        const y = height / 2.0;
+
+        const ogFont = ctx.font;
+
+        // Draw the clock face
+        ctx.beginPath();
+        ctx.fillStyle = this.faceColor;
+        ctx.arc(x, y, radius, 0, 360);
+        ctx.fill();
+        ctx.closePath();
+        
+        // Draw the clock numbers
+        ctx.fillStyle = this.numeralColor;
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.font = '18pt Arial';
+        circlePoints(x, y, radius * 0.9, 12, (x, y, index) => {
+            const numeral = (index + 2) % 12 + 1;
+            ctx.fillText(numeral, x, y);
+        });
+
+        // Draw the clock hands
+        /** @param {CanvasRenderingContext2D} ctx */
+        function renderHand(ctx, x, y, length, progress) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            const [x2, y2] = circlePoint(x, y, length, (2 * Math.PI * progress) - Math.PI / 2);
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+            ctx.closePath();
+        }
+
+        // Draw current hand
+        const date = new Date();        
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 10;
+        renderHand(ctx, x, y, radius * 0.8, date.getMinutes() / 60);
+        renderHand(ctx, x, y, radius * 0.5, date.getHours() / 12);
+        ctx.strokeStyle = '#FF0000';
+        ctx.lineWidth = 1;
+
+        if (this.secondMode === TimePieceSecondMode.Smooth) {
+            renderHand(ctx, x, y, radius, ((date.getSeconds() * 1000) + date.getMilliseconds()) / 60000);
+        } else if (this.secondMode === TimePieceSecondMode.Jittery) {
+            const now = date.getSeconds() / 60;
+            const next = (date.getSeconds() + 1) / 60;
+            const progress  = date.getMilliseconds() / 1000;
+            const jitter    = easeInBack(progress) * (next - now);
+            renderHand(ctx, x, y, radius, now + jitter);
+        } else {
+            renderHand(ctx, x, y, radius, date.getSeconds() / 60);
+        }
+
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, 360);
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.fillStyle = 'black';
+        ctx.font = ogFont;
+        ctx.textAlign = 'left';
+        ctx.textBaseline = 'bottom';
+        ctx.fillText("Frame: " + this.frame, 0, height);
+        ctx.fillText("Now: " + (new Date()).toLocaleString(), 0, height - 10);
     }
 
     /** Queues the frame for rendering  */
@@ -67,6 +154,7 @@ class TimePiece {
         
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
+        this.frame = 0;
 
         // Kick off the rendering
         this.queueFrame();
@@ -78,5 +166,17 @@ class TimePiece {
         if (this._request !== false)
             window.cancelAnimationFrame(this._request);
     }
+}
 
+function circlePoints(x, y, radius, n, callback) {
+    for (let i = 0; i < n; i++) {
+        const [ x2, y2 ] = circlePoint(x, y, radius, 2 * Math.PI * i / n);
+        callback(x2, y2, i);
+    }
+}
+
+function circlePoint(x, y, radius, radians) {
+    const x2 = x + radius * Math.cos(radians);
+    const y2 = y + radius * Math.sin(radians);
+    return [ x2, y2 ];
 }
